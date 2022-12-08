5c3385583dba742bdef92cbf80a2c7bb6933d9d5
benezoltan <90574145+benezoltan@users.noreply.github.com>
2022-12-08 09:42:55 +0100

    Add files via upload

---
+++ HF46o13/HF46o13/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace HF46o13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.szoveg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eredmeny_a = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.eredmeny_b = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.eredmeny_c = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.eredmeny_d = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.eredmeny_e = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.eredmeny_f = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.eredmeny_g = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.eredmeny_h = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // szoveg
+            // 
+            this.szoveg.Location = new System.Drawing.Point(218, 37);
+            this.szoveg.Name = "szoveg";
+            this.szoveg.Size = new System.Drawing.Size(571, 26);
+            this.szoveg.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Szöveg:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "a. Minden második betű";
+            // 
+            // eredmeny_a
+            // 
+            this.eredmeny_a.Location = new System.Drawing.Point(218, 82);
+            this.eredmeny_a.Name = "eredmeny_a";
+            this.eredmeny_a.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_a.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b. Minden 5. betű";
+            // 
+            // eredmeny_b
+            // 
+            this.eredmeny_b.Location = new System.Drawing.Point(218, 130);
+            this.eredmeny_b.Name = "eredmeny_b";
+            this.eredmeny_b.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_b.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "c. Utolsó 5 betű";
+            // 
+            // eredmeny_c
+            // 
+            this.eredmeny_c.Location = new System.Drawing.Point(218, 175);
+            this.eredmeny_c.Name = "eredmeny_c";
+            this.eredmeny_c.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_c.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "d. Fordított szöveg";
+            // 
+            // eredmeny_d
+            // 
+            this.eredmeny_d.Location = new System.Drawing.Point(220, 212);
+            this.eredmeny_d.Name = "eredmeny_d";
+            this.eredmeny_d.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_d.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "e. Szavak fordítva";
+            // 
+            // eredmeny_e
+            // 
+            this.eredmeny_e.Location = new System.Drawing.Point(218, 255);
+            this.eredmeny_e.Name = "eredmeny_e";
+            this.eredmeny_e.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_e.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "f. Van benne t betű?";
+            // 
+            // eredmeny_f
+            // 
+            this.eredmeny_f.Location = new System.Drawing.Point(220, 297);
+            this.eredmeny_f.Name = "eredmeny_f";
+            this.eredmeny_f.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_f.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(33, 341);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "g. Van benne:  \"fa\"";
+            // 
+            // eredmeny_g
+            // 
+            this.eredmeny_g.Location = new System.Drawing.Point(220, 341);
+            this.eredmeny_g.Name = "eredmeny_g";
+            this.eredmeny_g.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_g.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(33, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(169, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "h. Hány magánhangzó";
+            // 
+            // eredmeny_h
+            // 
+            this.eredmeny_h.Location = new System.Drawing.Point(220, 382);
+            this.eredmeny_h.Name = "eredmeny_h";
+            this.eredmeny_h.Size = new System.Drawing.Size(571, 26);
+            this.eredmeny_h.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 506);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.eredmeny_h);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.eredmeny_g);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.eredmeny_f);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.eredmeny_e);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.eredmeny_d);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.eredmeny_c);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.eredmeny_b);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.eredmeny_a);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.szoveg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox szoveg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox eredmeny_a;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox eredmeny_b;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox eredmeny_c;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox eredmeny_d;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox eredmeny_e;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox eredmeny_f;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox eredmeny_g;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox eredmeny_h;
+    }
+}
+
---
+++ HF46o13/HF46o13/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HF46o13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string MindenXedik(string s,int hanyadik)  // ABCDEFG
+        {
+            string s2 = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ( (i + 1) % hanyadik == 0)
+                {
+                    s2 = s2 + s[i];
+                }
+            }
+
+            return s2;  
+        }
+
+        string Forditva(string s)
+        {
+            string s2 = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                s2 = s[i] + s2;
+            }
+            return s2;
+        }
+
+        int HanyMaganhangzo(string s)
+        {   string maganhangzo = "aAáÁeEéÉiIíÍoOóÓöÖőŐuUúÚüÜűŰ";
+            int db = 0;
+            foreach (var betu in s)
+            {
+                if (maganhangzo.IndexOf(betu) >= 0 ) db++;  
+            }
+            return db;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eredmeny_a.Text = MindenXedik(szoveg.Text,2);
+            eredmeny_b.Text = MindenXedik(szoveg.Text,5);
+            // C.feladat: ABCDEFGH ... hossz
+            int startIndex = szoveg.Text.Length-5;
+            if (startIndex < 0)
+                eredmeny_c.Text = "Nincs 5 betű! "+szoveg.Text;
+            else
+                eredmeny_c.Text = szoveg.Text.Substring(startIndex);
+
+            eredmeny_d.Text = Forditva(szoveg.Text);
+            //E. feladat
+            string[] szavak = szoveg.Text.Split(' '); // szétvágja
+
+            foreach (string szo in szavak)
+            {
+                eredmeny_e.Text = szo +" "+ eredmeny_e.Text;
+            }
+
+            // F feladat
+            if (szoveg.Text.IndexOf('t') < 0)
+                eredmeny_f.Text = "Nincs benne t betű";
+            else
+                eredmeny_f.Text = "Van benne t betű. "+ (szoveg.Text.IndexOf('t')+1) +".betű";
+            // G feladat
+            if (szoveg.Text.LastIndexOf("fa") < 0)
+                eredmeny_g.Text = "Nincs benne a fa";
+            else
+                eredmeny_g.Text = "Van benne fa " + (szoveg.Text.LastIndexOf("fa") + 1) + ". kezd";
+
+            // H hány magánhangzó?
+            eredmeny_h.Text = HanyMaganhangzo(szoveg.Text).ToString();
+    }
+    }
+}
